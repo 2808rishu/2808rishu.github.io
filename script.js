db530e36d1301a69f58e19dcd1954b2b8b58f523
2808rishu <152893212+2808rishu@users.noreply.github.com>
2025-07-17 21:39:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// Wait for the entire webpage to load before running the script
-document.addEventListener('DOMContentLoaded', () => {
+const body = document.body;
+const handle = document.getElementById('handle');
 
-    // Get the HTML elements we need to interact with
-    const pullString = document.getElementById('pull-string');
-    const body = document.body;
-
-    // Listen for a click event on the pull-string element
-    pullString.addEventListener('click', () => {
-        // When clicked, toggle the 'light' and 'dark' classes on the body
-        body.classList.toggle('light');
-        body.classList.toggle('dark');
-    });
-
+handle.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  body.classList.toggle('light');
 });
